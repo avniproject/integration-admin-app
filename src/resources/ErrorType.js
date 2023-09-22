1fef24844353c385b5fe5ad2f4e166ec9864b472
himeshr <himeshl@samanvayfoundation.org>
2023-09-22 11:03:03 +0530

    avniproject/integration-service#80 | Allow creation of GoonjErrorTypes from admin app

--- src/resources/ErrorType.js
+++ src/resources/ErrorType.js
@@ -1,19 +1,14 @@
-import {
-    Datagrid,
-    Edit,
-    List,
-    SimpleForm,
-    TextField,
-    TextInput,
-    Create, required,
-} from "react-admin";
+import {Create, Datagrid, Edit, List, required, SelectInput, SimpleForm, TextField, TextInput,} from "react-admin";
 import React from "react";
 
 export const ErrorTypeList = (props) => (
     <List {...props}>
         <Datagrid>
-            <TextField source="id" sortable={true}/>
-            <TextField source="name" sortable={true}/>
+            <TextField source="id" sortable={false}/>
+            <TextField source="name" sortable={false}/>
+            <TextField source="comparisonOperator" sortable={false}/>
+            <TextField source="comparisonValue" sortable={false}/>
+            <TextField source="followUpStep" sortable={false}/>
         </Datagrid>
     </List>
 );
@@ -22,6 +17,22 @@ let getForm = function (isEdit) {
     return <SimpleForm>
         {isEdit && <TextField source="id"/>}
         <TextInput source="name" type="name" validate={[required("Mandatory")]} name="name"/>
+        <SelectInput label="Comparison Operator"
+          choices={[
+              { id: 'EQUALS', name: 'EQUALS' },
+              { id: 'CONTAINS', name: 'CONTAINS' },
+              { id: 'MATCHES', name: 'MATCHES' },
+          ]}
+          source="comparisonOperator" name="comparisonOperator" validate={[required("Mandatory")]}>
+        </SelectInput>
+      <TextInput source="comparisonValue" type="comparisonValue" validate={[required("Mandatory")]} name="comparisonValue"/>
+      <SelectInput label="Follow Up Step"
+                   choices={[
+                     { id: 'Process', name: 'Process' },
+                     { id: 'Terminal', name: 'Terminal' },
+                   ]}
+                   source="followUpStep" name="followUpStep" validate={[required("Mandatory")]}>
+      </SelectInput>
     </SimpleForm>;
 };
 
